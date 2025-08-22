dd47e0eba7d2daf427549860b8559a8c0365edd9
saurav <saurabh.shr99@gmail.com>
2025-08-22 21:22:28 +0530

    dark mode true by default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { cn } from "../lib/utils";
 
 const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
